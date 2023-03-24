014affedce201fef9512e2b86fb067f4ed137369
Kimberly Dijkmans <kim.14420@outlook.com>
2023-03-24 15:40:58 +0100

    created route to add new series

--- api/series.js
+++ api/series.js
@@ -36,3 +36,34 @@ seriesRouter.get('/', (req, res, next) => {
 seriesRouter.get('/:seriesId', (req, res, next) => {
     res.status(200).send({series: req.series});
 });
+
+
+const validateSeries = (req, res, next) => {
+    const toCreateSeries = req.body.series;
+    if (!toCreateSeries.name || !toCreateSeries.description) {
+        return res.status(400).send();
+    }
+    next();
+};
+
+seriesRouter.post('/', validateSeries, (req, res, next) => {
+    const toCreateSeries = req.body.series;
+    db.run('INSERT INTO Series (name, description) VALUES ($name, $description)', {
+        $name: toCreateSeries.name,
+        $description: toCreateSeries.description
+    }, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        db.get('SELECT * FROM Series WHERE id = $id', {
+            $id: this.lastID
+        }, (err, series) => {
+            if (err) {
+                next(err);
+            } else if (!series) {
+                return res.status(500).send();
+            }
+            res.status(201).send({series: series});
+        });
+    });
+});
