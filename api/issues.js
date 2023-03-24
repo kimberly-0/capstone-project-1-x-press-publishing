d5019f70a3b6c54a2d311995d186d8ab52b2a0f6
Kimberly Dijkmans <kim.14420@outlook.com>
2023-03-24 17:28:17 +0100

    created route to add a new issue

--- api/issues.js
+++ api/issues.js
@@ -17,3 +17,48 @@ issuesRouter.get('/', (req, res, next) => {
         }
     });
 });
+
+const validateIssue = (req, res, next) => {
+    const toCreateIssue = req.body.issue;
+    if (!toCreateIssue.name || !toCreateIssue.issueNumber || !toCreateIssue.publicationDate || !toCreateIssue.artistId) {
+        return res.status(400).send();
+    }
+    // Check if artist exists
+    db.get('SELECT * FROM Artist WHERE id = $id', {
+        $id: toCreateIssue.artistId
+    }, (err, artist) => {
+        if (err) {
+            next(err);
+        } else if (artist) {
+            req.artist = artist;
+            next();
+        } else {
+            res.status(400).send();
+        }
+    });
+};
+
+issuesRouter.post('/', validateIssue, (req, res, next) => {
+    const toCreateIssue = req.body.issue;
+    db.run('INSERT INTO Issue (name, issue_number, publication_date, artist_id, series_id) VALUES ($name, $issueNumber, $publicationDate, $artistId, $seriesId)', {
+        $name: toCreateIssue.name,
+        $issueNumber: toCreateIssue.issueNumber,
+        $publicationDate: toCreateIssue.publicationDate, 
+        $artistId: toCreateIssue.artistId, 
+        $seriesId: req.series.id
+    }, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        db.get('SELECT * FROM Issue WHERE id = $id', {
+            $id: this.lastID
+        }, (err, issue) => {
+            if (err) {
+                next(err);
+            } else if (!issue) {
+                return res.status(500).send();
+            }
+            res.status(201).send({issue: issue});
+        });
+    });
+});
