2ec538f53050ceaed19bd35c0a878a828e401110
Kimberly Dijkmans <kim.14420@outlook.com>
2023-03-24 15:40:33 +0100

    created route to get single a series by id

--- api/series.js
+++ api/series.js
@@ -8,6 +8,21 @@ module.exports = seriesRouter;
 const issuesRouter = require('./issues');
 seriesRouter.use('/:seriesId/issues', issuesRouter);
 
+seriesRouter.param('seriesId', (req, res, next, id) => {
+    db.get('SELECT * FROM Series WHERE id = $id', {
+        $id: Number(id)
+    }, (err, series) => {
+        if (err) {
+            next(err);
+        } else if (series) {
+            req.series = series;
+            next();
+        } else {
+            res.status(404).send();
+        }
+    });
+});
+
 seriesRouter.get('/', (req, res, next) => {
     db.all('SELECT * FROM Series', (err, series) => {
         if (err) {
@@ -16,4 +31,8 @@ seriesRouter.get('/', (req, res, next) => {
             res.status(200).json({series: series});
         }
     });
+});
+
+seriesRouter.get('/:seriesId', (req, res, next) => {
+    res.status(200).send({series: req.series});
 });
