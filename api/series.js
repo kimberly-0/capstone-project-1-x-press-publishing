a1f415d4c636a5120dfa247f9cc87e51fcb39439
Kimberly Dijkmans <kim.14420@outlook.com>
2023-03-24 15:39:41 +0100

    created route to get all series

--- api/series.js
+++ api/series.js
@@ -7,3 +7,13 @@ module.exports = seriesRouter;
 
 const issuesRouter = require('./issues');
 seriesRouter.use('/:seriesId/issues', issuesRouter);
+
+seriesRouter.get('/', (req, res, next) => {
+    db.all('SELECT * FROM Series', (err, series) => {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json({series: series});
+        }
+    });
+});
