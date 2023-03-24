415904146d61c46b3e368357611cba525945657d
Kimberly Dijkmans <kim.14420@outlook.com>
2023-03-24 15:37:49 +0100

    imported and mounted issues router

--- api/series.js
+++ api/series.js
@@ -4,3 +4,6 @@ const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'
 
 const seriesRouter = express.Router();
 module.exports = seriesRouter;
+
+const issuesRouter = require('./issues');
+seriesRouter.use('/:seriesId/issues', issuesRouter);
