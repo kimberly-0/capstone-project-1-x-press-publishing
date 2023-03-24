dc44934150605a168ead6a0f0b23212a773e6c29
Kimberly Dijkmans <kim.14420@outlook.com>
2023-03-24 15:36:21 +0100

    mounted series router to api router

--- api/api.js
+++ api/api.js
@@ -5,5 +5,8 @@ const apiRouter = express.Router();
 module.exports = apiRouter;
 
 const artistsRouter = require('./artists');
-
 apiRouter.use('/artists', artistsRouter);
+
+const seriesRouter = require('./series');
+apiRouter.use('/series', seriesRouter);
+
